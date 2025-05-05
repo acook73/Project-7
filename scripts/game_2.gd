11412dd7415d9daeee263753451205c83e303f38
BuildTools <unconfigured@null.spigotmc.org>
2025-05-05 16:49:23 -0400

    treant merged, fog parallax improvement, tutorial slight update

--- scripts/game_2.gd
+++ scripts/game_2.gd
@@ -5,6 +5,8 @@ extends Node2D
 func _physics_process(delta: float) -> void:
 	$Player/Camera2D2/TextureProgressBar.max_value = $Player.hp_max
 	$Player/Camera2D2/TextureProgressBar.value = $Player.hp
+	if ($Player.hp <= 0):
+		$Enemies/Treant.hp = 4000
 
 func _on_teleport_barrier_body_entered(body: Node2D) -> void:
 	body.set_position(body.lastSafePos)
